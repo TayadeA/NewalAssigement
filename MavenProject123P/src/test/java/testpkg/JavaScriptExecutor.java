6178eb6709a2c96af4cdd10a5bcb749eef40c08a
Admin <tayade1995akshay@gmail.com>
2023-12-09 18:15:06 +0530

    Akshay : JavaScriptExecutor class is updated :--->

--- MavenProject123P/src/test/java/testpkg/JavaScriptExecutor.java
+++ MavenProject123P/src/test/java/testpkg/JavaScriptExecutor.java
@@ -1,9 +1,17 @@
 package testpkg;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class JavaScriptExecutor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
 
 	}
 
