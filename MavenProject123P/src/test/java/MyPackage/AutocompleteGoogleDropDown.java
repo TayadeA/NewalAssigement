a6a740611d697c1c040866e909499aaa30b5a69d
Admin <tayade1995akshay@gmail.com>
2023-12-10 17:28:46 +0530

    akshy : text update

--- MavenProject123P/src/test/java/MyPackage/AutocompleteGoogleDropDown.java
+++ MavenProject123P/src/test/java/MyPackage/AutocompleteGoogleDropDown.java
@@ -1,22 +1,20 @@
 package MyPackage;
-
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-import testpkg.WebElement;
+
 
 public class AutocompleteGoogleDropDown {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
+		Thread.sleep(2000);
 		driver.get("https://www.twoplugs.com/");
 		driver.manage().window().maximize();
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("//a[text()='Live Posting']")).click();
 		
 		
